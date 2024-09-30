6b5f5ec758bb8f8bdc861adb653f4675d2695069
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-09-30 23:05:35 +0200

    adjusting broken components

--- skillforge/src/App.js
+++ skillforge/src/App.js
@@ -1,16 +1,10 @@
 // src/App.js
 import React from "react";
-import Accordion from "./components/accordion";
-import Carousel from "./components/carousel";
-import myGrid from "./components/myGrid";
 
 function App() {
   return (
     <div>
       <h1>Welcome to My App</h1>
-      <Carousel />
-      <Accordion />
-      <myGrid />
     </div>
   );
 }
