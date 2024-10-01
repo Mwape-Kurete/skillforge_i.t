21ebd4096381107bab98d9f01b1dc5474d29c62d
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-10-01 14:58:39 +0200

    styling

--- skillforge/src/pages/about.js
+++ skillforge/src/pages/about.js
@@ -2,36 +2,15 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import NavbarComponent from "../Components/NavbarComponent";
-import Hero from "../Components/Hero";
+
 import FooterComponent from "../Components/FooterComponent";
 import WhyUs from "../Components/WhyUs";
 import MeetTheTeam from "../Components/MeetTheTeam";
 
 function About() {
-  const heroData = {
-    "/": {
-      title: "SkillForge I.T",
-      subtitle: "Your journey to mastering IT starts here!",
-    },
-    "/about": {
-      title: "About Us",
-      subtitle: "Learn more about our mission and team.",
-    },
-    "/courses": {
-      title: "Explore Our Courses",
-      subtitle: "Master essential IT skills with our bootcamps.",
-    },
-  };
-
-  const currentPage = window.location.pathname;
-
   return (
     <Container>
       <NavbarComponent />
-      <Hero
-        title={heroData[currentPage]?.title || "Default Title"} // Add fallback if heroData[currentPage] is undefined
-        subtitle={heroData[currentPage]?.subtitle || "Default Subtitle"}
-      />
       <br />
       <WhyUs />
       <br />
--- skillforge/src/pages/courses.js
+++ skillforge/src/pages/courses.js
@@ -3,31 +3,12 @@ import React from "react";
 
 import { Container, Row, Col } from "react-bootstrap";
 import NavbarComponent from "../Components/NavbarComponent";
-import Hero from "../Components/Hero";
 import FooterComponent from "../Components/FooterComponent.js";
 
 import CourseCards from "../Components/CourseCards.js";
-import { CardGroup } from "react-bootstrap";
 import "../Styles/courses.css";
 
 function Courses() {
-  const heroData = {
-    "/": {
-      title: "SkillForge I.T",
-      subtitle: "Your journey to mastering IT starts here!",
-    },
-    "/about": {
-      title: "About Us",
-      subtitle: "Learn more about our mission and team.",
-    },
-    "/courses": {
-      title: "Explore Our Courses",
-      subtitle: "Master essential IT skills with our bootcamps.",
-    },
-  };
-
-  const currentPage = window.location.pathname;
-
   return (
     <Container>
       <NavbarComponent />
