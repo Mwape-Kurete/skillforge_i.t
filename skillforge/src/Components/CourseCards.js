ddd3e254f73d6da6359f24bc731e2187f0e51552
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-10-07 13:03:58 +0200

    styling progress

--- skillforge/src/Components/CourseCards.js
+++ skillforge/src/Components/CourseCards.js
@@ -3,11 +3,12 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import { Col, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 function CourseCards() {
   return (
     <Row className="courses-all-cont">
-      <Col md={6}>
+      <Col md={6} className="course-col">
         <Card className="course-cards">
           <Card.Header>A+ Bootcamp (4 Months)</Card.Header>
           <Card.Body>
@@ -35,106 +36,191 @@ function CourseCards() {
                 </li>
               </ol>
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <div className="mt-auto d-flex justify-content-center">
+              <Button className="enroll-now-cta">
+                <Link to="/enroll" className="link-text">
+                  Enroll Now
+                </Link>
+              </Button>
+            </div>
           </Card.Body>
         </Card>
       </Col>
 
-      <Col md={6}>
+      <Col md={6} className="course-col">
         <Card className="course-cards">
-          <Card.Header>A+ Bootcamp (4 Months)</Card.Header>
+          <Card.Header>Python Bootcamp (4 Months)</Card.Header>
           <Card.Body>
             <Card.Title>
-              This bootcamp prepares students for the CompTIA A+ certification
-              exam, focusing on hardware, networking, and troubleshooting.
+              Learn the fundamentals of programming with Python, one of the most
+              versatile languages in the industry.
             </Card.Title>
             <Card.Text>
               <ol>
                 <li>
-                  Month 1: Introduction to hardware components, system
-                  architecture.
+                  Month 1: Introduction to Python, data types, variables, and
+                  functions.
+                </li>
+                <li>Month 2: Loops, conditionals, and error handling.</li>
+                <li>
+                  Month 3: Working with files, data structures (lists,
+                  dictionaries, sets).
                 </li>
                 <li>
-                  Month 2: Networking essentials: IP addressing, routers, and
-                  switches.
-                </li>
-                <li>
-                  Month 3: Operating systems: Windows, Linux, and MacOS
-                  troubleshooting.
-                </li>
-                <li>
-                  Month 4: Security fundamentals, maintenance, and preparation
-                  for the CompTIA A+ exam.
+                  Month 4: Project work: Building a simple Python application
+                  (e.g., a calculator or data analysis tool).
                 </li>
               </ol>
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <div className="mt-auto d-flex justify-content-center">
+              <Button className="enroll-now-cta">
+                <Link to="/enroll" className="link-text">
+                  Enroll Now
+                </Link>
+              </Button>
+            </div>
           </Card.Body>
         </Card>
       </Col>
 
-      <Col md={6}>
+      <Col md={6} className="course-col">
         <Card className="course-cards">
-          <Card.Header>A+ Bootcamp (4 Months)</Card.Header>
+          <Card.Header>React Bootcamp (4 Months)</Card.Header>
           <Card.Body>
             <Card.Title>
-              This bootcamp prepares students for the CompTIA A+ certification
-              exam, focusing on hardware, networking, and troubleshooting.
+              Master front-end web development using React, one of the most
+              popular JavaScript libraries.
             </Card.Title>
             <Card.Text>
               <ol>
+                <li>Month 1: Introduction to JavaScript and ES6 basics.</li>
                 <li>
-                  Month 1: Introduction to hardware components, system
-                  architecture.
+                  Month 2: Introduction to React, components, and state
+                  management.
                 </li>
                 <li>
-                  Month 2: Networking essentials: IP addressing, routers, and
-                  switches.
+                  Month 3: Handling events, forms, and making HTTP requests.
                 </li>
                 <li>
-                  Month 3: Operating systems: Windows, Linux, and MacOS
-                  troubleshooting.
-                </li>
-                <li>
-                  Month 4: Security fundamentals, maintenance, and preparation
-                  for the CompTIA A+ exam.
+                  Month 4: Project work: Building a dynamic React-based web
+                  application.
                 </li>
               </ol>
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <div className="mt-auto d-flex justify-content-center">
+              <Button className="enroll-now-cta">
+                <Link to="/enroll" className="link-text">
+                  Enroll Now
+                </Link>
+              </Button>
+            </div>
           </Card.Body>
         </Card>
       </Col>
 
-      <Col md={6}>
+      <Col md={6} className="course-col">
         <Card className="course-cards">
-          <Card.Header>A+ Bootcamp (4 Months)</Card.Header>
+          <Card.Header>ICDL Bootcamp (4 Months)</Card.Header>
           <Card.Body>
             <Card.Title>
-              This bootcamp prepares students for the CompTIA A+ certification
-              exam, focusing on hardware, networking, and troubleshooting.
+              The ICDL bootcamp provides students with the skills to use
+              essential computer applications effectively.
             </Card.Title>
             <Card.Text>
               <ol>
                 <li>
-                  Month 1: Introduction to hardware components, system
-                  architecture.
+                  Month 1: Introduction to IT concepts and file management.
+                </li>
+                <li>Month 2: Word processing (MS Word, Google Docs).</li>
+                <li>
+                  Month 3: Spreadsheets and data analysis (MS Excel, Google
+                  Sheets).
                 </li>
                 <li>
-                  Month 2: Networking essentials: IP addressing, routers, and
-                  switches.
-                </li>
-                <li>
-                  Month 3: Operating systems: Windows, Linux, and MacOS
-                  troubleshooting.
-                </li>
-                <li>
-                  Month 4: Security fundamentals, maintenance, and preparation
-                  for the CompTIA A+ exam.
+                  Month 4: Presentations and databases (MS PowerPoint, Google
+                  Slides, MS Access).
                 </li>
               </ol>
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <div className="mt-auto d-flex justify-content-center">
+              <Button className="enroll-now-cta">
+                <Link to="/enroll" className="link-text">
+                  Enroll Now
+                </Link>
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+
+      <Col md={6} className="course-col">
+        <Card className="course-cards">
+          <Card.Header>Java Bootcamp (4 Months)</Card.Header>
+          <Card.Body>
+            <Card.Title>
+              Learn Java programming, an essential language for enterprise-level
+              applications and Android development.
+            </Card.Title>
+            <Card.Text>
+              <ol>
+                <li>
+                  Month 1: Introduction to Java, setting up the environment, and
+                  basic syntax.
+                </li>
+                <li>Month 2: Object-oriented programming (OOP) concepts.</li>
+                <li>
+                  Month 3: Working with files, collections, and error handling.
+                </li>
+                <li>
+                  Month 4: Project work: Building a Java-based application
+                  (e.g., a to-do list app).
+                </li>
+              </ol>
+            </Card.Text>
+            <div className="mt-auto d-flex justify-content-center">
+              <Button className="enroll-now-cta">
+                <Link to="/enroll" className="link-text">
+                  Enroll Now
+                </Link>
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={6} className="course-col">
+        <Card className="course-cards">
+          <Card.Header>Project Management Bootcamp (4 Months)</Card.Header>
+          <Card.Body>
+            <Card.Title>
+              This bootcamp provides foundational knowledge and skills in
+              managing IT projects, aligned with the PMP certification.
+            </Card.Title>
+            <Card.Text>
+              <ol>
+                <li>
+                  Month 1: Introduction to project management frameworks and
+                  methodologies (Agile, Waterfall).
+                </li>
+                <li>
+                  Month 2: Project planning, scheduling, and resource
+                  management.
+                </li>
+                <li>
+                  Month 3: Risk management, budgeting, and quality assurance.
+                </li>
+                <li>
+                  Month 4: Project work: Managing a simulated IT project using
+                  learned skills.
+                </li>
+              </ol>
+            </Card.Text>
+            <div className="mt-auto d-flex justify-content-center">
+              <Button className="enroll-now-cta">
+                <Link to="/enroll" className="link-text">
+                  Enroll Now
+                </Link>
+              </Button>
+            </div>
           </Card.Body>
         </Card>
       </Col>
--- skillforge/src/Components/FormComp.js
+++ skillforge/src/Components/FormComp.js
@@ -3,48 +3,61 @@ import Button from "react-bootstrap/Button";
 import { Row, Col } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
+import "../Styles/form.css";
+
 function FormComp() {
   return (
-    <Form>
-      <Row className="mb-3">
-        <Form.Group as={Col} className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Name</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
+    <div className="d-flex justify-content-center align-items-center">
+      <Form className="form-">
+        <Row className="mb-3">
+          <Form.Group as={Col} className="mb-3" controlId="formGridAddress1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control placeholder="John Doe" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control type="number" placeholder="phone number" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group>
+          <Form.Select aria-label="Default select example">
+            <option>Select your course</option>
+            <option value="1">A+ Bootcamp</option>
+            <option value="2">Python Bootcamp</option>
+            <option value="3">React Bootcamp</option>
+            <option value="4">ICDL Bootcamp</option>
+            <option value="3">Java Bootcamp</option>
+            <option value="3">Project Management Bootcamp</option>
+          </Form.Select>
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={5}
+            placeholder="have a question? let us know here"
+          />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Phone</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group className="mb-3" id="formGridCheckbox">
+          <Form.Check type="checkbox" label="remember me for next time!" />
         </Form.Group>
-      </Row>
 
-      <Form.Group>
-        <Form.Select aria-label="Default select example">
-          <option>Open this select menu</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </Form.Select>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="remember me for next time!" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <div className="d-flex justify-content-center align-items-center">
+          <Button className="enroll-now-cta" type="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
   );
 }
 
